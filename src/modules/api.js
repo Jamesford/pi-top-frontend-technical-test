ad5ff3b06ec5da93f0f23e682e25150e453b1d78
Jamesford <jamie@stobbs.me>
2019-09-05 14:47:22 +0200

    Add fetch functions for api

---
+++ src/modules/api.js
@@ -0,0 +1,40 @@
+const API_HOST = 'https://backend.pi-top.com/todo-test/v1'
+
+export const getTodos = () => fetch(`${API_HOST}/todos`)
+
+export const getTodoById = id => fetch(`${API_HOST}/todos/${id}`)
+
+export const createTodo = ({ title, description, priority, tags }) =>
+  fetch(`${API_HOST}/todos`, {
+    method: 'POST',
+    body: JSON.stringify({ title, description, priority, tags }),
+    headers: {
+      'content-type': 'application/json'
+    }
+  })
+
+export const updateTodo = (id, isDone) =>
+  fetch(`${API_HOST}/todos/${id}`, {
+    method: 'PUT',
+    body: JSON.stringify({ isDone }),
+    headers: {
+      'content-type': 'application/json'
+    }
+  })
+
+export const resetTodos = () =>
+  fetch(`${API_HOST}/reset`, {
+    method: 'POST',
+    body: JSON.stringify({}),
+    headers: {
+      'content-type': 'application/json'
+    }
+  })
+
+export default {
+  getTodos,
+  getTodoById,
+  createTodo,
+  updateTodo,
+  resetTodos
+}
