3b0874e477fc224096b076b64408785eb439624b
Jamesford <jamie@stobbs.me>
2019-09-06 04:48:50 +0200

    Implement filtering by isDone, sort by priority

--- src/components/TodosList/index.js
+++ src/components/TodosList/index.js
@@ -5,33 +5,57 @@ import { getTodos, resetTodos } from '../../modules/reducers/todos'
 import LoadingIndicator from '../LoadingIndicator'
 import TodosListItem from '../TodosListItem'
 
+const filters = [
+  { text: 'All', func: () => true },
+  { text: 'Done', func: ({ isDone }) => isDone },
+  { text: 'Doing', func: ({ isDone }) => !isDone }
+]
+
+const sorters = [
+  { text: '⬇️', func: ({ priority: a }, { priority: b }) => b - a },
+  { text: '⬆️', func: ({ priority: a }, { priority: b }) => a - b }
+]
+
 export class TodosList extends Component {
+  state = {
+    filter: 0,
+    sort: 0
+  }
+
   componentDidMount() {
     const { actions } = this.props
     actions.getTodos()
   }
 
+  toggleFilter = () => {
+    this.setState(state => ({
+      filter: state.filter > 1 ? 0 : state.filter + 1
+    }))
+  }
+
+  toggleSort = () => {
+    this.setState(state => ({ sort: state.sort ? 0 : 1 }))
+  }
+
   render() {
     const { loading, error, todos } = this.props
+    const { filter, sort } = this.state
 
     return (
       <section className="flex-col mx-auto max-w-sm w-full">
-        <div className="mb-2 flex justify-between">
+        <div className="mb-2 flex justify-end">
           <div className="flex">
-            <span className="bg-white border-gray-400 border text-gray-800 font-bold py-2 px-4 rounded-l cursor-default">
-              Priority
-            </span>
-            <button className="bg-white border-gray-400 border-t border-b hover:bg-gray-100 text-gray-800 font-bold py-2 px-4">
-              ⬆️
+            <button
+              className="bg-white border-gray-400 border border-r-0 hover:bg-gray-100 text-gray-800 font-bold py-2 px-4 rounded-l"
+              onClick={this.toggleSort}
+            >
+              Priority: {sorters[sort].text}
             </button>
-            <button className="bg-white border-gray-400 border hover:bg-gray-100 text-gray-800 font-bold py-2 px-4 rounded-r">
-              ⬇️
-            </button>
-          </div>
-
-          <div>
-            <button className="bg-white border-gray-400 border hover:bg-gray-100 text-gray-800 font-bold py-2 px-4 rounded">
-              Filter: All
+            <button
+              className="bg-white border-gray-400 border hover:bg-gray-100 text-gray-800 font-bold py-2 px-4 rounded-r"
+              onClick={this.toggleFilter}
+            >
+              Filter: {filters[filter].text}
             </button>
           </div>
         </div>
@@ -39,20 +63,31 @@ export class TodosList extends Component {
         {loading && todos.length === 0 && <LoadingIndicator />}
 
         <div className="bg-white flex-col shadow-lg rounded max-w-sm w-full">
-          {todos.map(
-            ({ id, title, description, createdAt, priority, tags, isDone }) => (
-              <TodosListItem
-                key={id}
-                id={id}
-                title={title}
-                description={description}
-                createdAt={createdAt}
-                priority={priority}
-                tags={tags}
-                isDone={isDone}
-              />
-            )
-          )}
+          {todos
+            .filter(filters[filter].func)
+            .sort(sorters[sort].func)
+            .map(
+              ({
+                id,
+                title,
+                description,
+                createdAt,
+                priority,
+                tags,
+                isDone
+              }) => (
+                <TodosListItem
+                  key={id}
+                  id={id}
+                  title={title}
+                  description={description}
+                  createdAt={createdAt}
+                  priority={priority}
+                  tags={tags}
+                  isDone={isDone}
+                />
+              )
+            )}
         </div>
 
         <div className="text-center mt-5">
