d03e0cec8ee2b631dd3ca23213259f2ee543aba3
Jamesford <jamie@stobbs.me>
2019-09-06 06:31:33 +0200

    Remove component unit tests

--- src/components/Layout/Layout.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { shallow } from 'enzyme'
-import Layout from '.'
-
-test('Layout contains main dom nodes', () => {
-  const layout = shallow(<Layout />)
-
-  expect(layout.find('h1').text()).toBe('Pi-Top Todo')
-})
